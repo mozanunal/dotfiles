8785e2fc23367a5dff868a19decdc9224133f576
mozanunal <mehmetozanunal@gmail.com>
2023-08-31 23:51:26 +0300

    start script updated

--- dev/start.py
+++ dev/start.py
@@ -9,3 +9,21 @@ from pyspark.sql.dataframe import DataFrame
 from pyspark.sql.session import SparkSession
 spark = SparkSession.builder.getOrCreate()
 
+pdf = pd.read_csv("~/dotfiles/dev/data/homes.csv")
+
+schema = T.StructType([
+    T.StructField("rowid", T.IntegerType()),
+    T.StructField("owner_name", T.StringType()),
+    T.StructField("area", T.StringType()),
+    T.StructField("value", T.IntegerType())
+])
+
+df = (
+    spark.read
+    .option("header", True)
+    .option("schema", schema)
+    .csv("/home/moz/dotfiles/dev/data/homes.csv")
+)
+df.show(2)
+
+
