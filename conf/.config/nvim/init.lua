3be66e21719454d42e848d59dfeacf0e4b04ba43
mozanunal <moz@pop-os.localdomain>
2024-08-04 19:51:08 +0300

    b16 colorschemes

--- conf/.config/nvim/init.lua
+++ conf/.config/nvim/init.lua
@@ -479,6 +479,23 @@ vim.o.shiftwidth = 2
 vim.o.tabstop = 2
 vim.o.formatoptions = "tcqj" -- j1croql or tcqj
 vim.o.laststatus = 3
-vim.cmd([[colorscheme cattpuccin]])
+
+
+local parse_b16 = function(str_in)
+	local colors = {}
+	for line in str_in:gmatch("[^\r\n]+") do
+		local key, value = line:match('(%w+):%s+"(.-)"')
+		if key and value then
+			colors[key] = "#" .. value
+		end
+	end
+	return colors
+end
+
+Set_b16_colors = function (str_in)
+  require('mini.base16').setup({ palette = parse_b16(str_in)})
+  vim.api.nvim_set_hl(0, 'WinSeparator', { bg = 'None' })
+end
+vim.cmd([[colorscheme b16_catppuccin_macchiato]])
 MiniMisc.setup_termbg_sync()
 -- vim: ts=2 sts=2 sw=2 et
