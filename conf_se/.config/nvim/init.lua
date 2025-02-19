29221ef7b47f75bd1a3cd8efea040266e2e9ef51
mozanunal <mehmetozanunal@gmail.com>
2025-02-19 20:45:15 +0300

    blink nvim new version update

--- conf_se/.config/nvim/init.lua
+++ conf_se/.config/nvim/init.lua
@@ -57,12 +57,12 @@ require("lazy").setup({
     opts = {
       keymap = { preset = "enter" },
       appearance = {
-        use_nvim_cmp_as_default = true,
+        use_nvim_cmp_as_default = false,
         nerd_font_variant = "mono",
       },
+      cmdline = { enabled = false, },
       sources = {
         default = { "lsp", "path", "snippets", "buffer" },
-        cmdline = {},
       },
       signature = { enabled = true },
     },
