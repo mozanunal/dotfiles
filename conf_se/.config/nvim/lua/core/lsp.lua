e6f405229665cd3e913583d91760c80428926513
mozanunal <mehmetozanunal@gmail.com>
2025-06-19 20:44:25 +0300

    nvim diagnostic simplified

--- conf_se/.config/nvim/lua/core/lsp.lua
+++ conf_se/.config/nvim/lua/core/lsp.lua
@@ -1,12 +1,17 @@
 vim.diagnostic.config({
-  virtual_text = false, -- completely off (will use virtual_lines instead)
-  virtual_lines = { -- new handler
-    only_current_line = true, -- show virtual lines only under cursor
-  },
-  severity_sort = true, -- most severe first
-  signs = true,
+  virtual_text = { current_line = true, virt_text_pos = "right_align" },
+  virtual_lines = false,
   underline = true,
-  update_in_insert = false,
+  signs = {
+    priority = 20,
+    text = {
+      [vim.diagnostic.severity.ERROR] = '✘',
+      [vim.diagnostic.severity.WARN] = '▲',
+      [vim.diagnostic.severity.INFO] = '•',
+      [vim.diagnostic.severity.HINT] = '»',
+    },
+  },
+  severity_sort = { reverse = false },
 })
 
 vim.lsp.enable({ 'terraformls', 'lua_ls', 'gopls', 'pyright', 'ruff', 'rust_analyzer', 'html', 'deno' })
